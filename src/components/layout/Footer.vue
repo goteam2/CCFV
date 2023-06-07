7325e919ee19db28cfc7b8546fd61ccd0dd64303
SPN <10730985+splesanaden@users.noreply.github.com>
2023-06-07 16:58:00 -0600

    updates!

--- src/components/layout/Footer.vue
+++ src/components/layout/Footer.vue
@@ -1,21 +1,38 @@
 <script setup>
-
-import links from '@/config/nav.js'
+import { ref } from "vue";
+import links from "@/config/nav.js";
+const submitted = ref(false);
+const submitForm = () => {
+  const form = document.getElementById("ccfv_signup_form");
+  form.submit();
+  submitted.value = true;
+};
 </script>
 <template>
-  <div class="ccfv-footer--about |  bg-ccfv-cyan | border-t-4 border-ccfv-orange">
+  <div
+    class="ccfv-footer--about | bg-ccfv-cyan | border-t-4 border-ccfv-orange"
+  >
     <div class="container mx-auto | grid grid-cols-12 |">
-      <div class=" col-span-12 lg:col-span-6 | py-12 p-8 lg:p-12">
+      <div class="col-span-12 lg:col-span-6 | py-12 p-8 lg:p-12">
         <div class="max-w-prose">
-          <h3 class="text-2xl lg:text-3xl font-bold font-display mb-3 lg:mb-3">Improving Health Through Vaccine Research</h3>
+          <h3 class="text-2xl lg:text-3xl font-bold font-display mb-3 lg:mb-3">
+            Improving Health Through Vaccine Research
+          </h3>
           <p class="lg:text-md xl:text-lg font-light mb-5">
-            The Canadian Center for Vaccinology (CCfV) in Halifax, Nova Scotia, is an integrated collaborative interdisciplinary vaccine research team committed to excellent research. CCfV unites the biomedical and clinical sciences with the social sciences and humanities to effectively span the research continuum from basic discovery to translation into useful vaccines to prevent disease in humans and to change population and public health outcomes.
+            The Canadian Center for Vaccinology (CCfV) in Halifax, Nova Scotia,
+            is an integrated collaborative interdisciplinary vaccine research
+            team committed to excellent research. CCfV unites the biomedical and
+            clinical sciences with the social sciences and humanities to
+            effectively span the research continuum from basic discovery to
+            translation into useful vaccines to prevent disease in humans and to
+            change population and public health outcomes.
           </p>
         </div>
       </div>
-      <div class="col-span-12 lg:col-span-6 | py-12 2xl:p-12 | flex items-center justify-center">
-        <div class="w-full 2xl:max-w-prose flex ">
-  
+      <div
+        class="col-span-12 lg:col-span-6 | py-12 2xl:p-12 | flex items-center justify-center"
+      >
+        <div class="w-full 2xl:max-w-prose flex">
           <a class="2xl:ms-auto hidden lg:flex" href="#">
             <img
               class="iwk-logo | w-32 mx-12 my-12 h-auto hover:opacity-80 self-end"
@@ -37,55 +54,126 @@ import links from '@/config/nav.js'
               alt="NSH Logo"
             />
           </a>
-          </div>
+        </div>
       </div>
     </div>
   </div>
-  <div class="ccfv-footer--main |  bg-ccfv-grey">
+  <div class="ccfv-footer--main | bg-ccfv-grey">
     <div class="container mx-auto | grid grid-cols-12 | py-12">
-      
-      <div class="col-span-12 lg:col-span-3  | xl:border-e-4 xl:border-white |  h-full | flex items-center">
-        <img class="ccfv-logo | w-84 mx-auto  h-auto" src="/img/ccfv-logo-collaboration.svg" alt="CCfV Logo" />
+      <div
+        class="col-span-12 lg:col-span-3 | xl:border-e-4 xl:border-white | h-full | flex items-center"
+      >
+        <img
+          class="ccfv-logo | w-84 mx-auto h-auto"
+          src="/img/ccfv-logo-collaboration.svg"
+          alt="CCfV Logo"
+        />
       </div>
-      <div class="col-span-12 lg:col-span-3 | p-12 | flex items-center justify-center | xl:border-e-4 xl:border-white">
-
+      <div
+        class="col-span-12 lg:col-span-3 | p-12 | flex items-center justify-center | xl:border-e-4 xl:border-white"
+      >
         <ul class="list-none">
           <li class="mb-4 lg:my-4" v-for="link in links">
-          <a :href="link.href" class="text-white hover:text-ccfv-orange text-lg font-bold">{{link.name}}</a>
+            <a
+              :href="link.href"
+              class="text-white hover:text-ccfv-orange text-lg font-bold"
+              >{{ link.name }}</a
+            >
           </li>
         </ul>
-       
       </div>
 
-      <div class="col-span-12 lg:col-span-6 | p-12 | flex items-center justify-center lg:justify-start |">
+      <div
+        class="col-span-12 lg:col-span-6 | p-12 | flex items-center justify-center lg:justify-start |"
+      >
         <div class="w-full 2xl:pe-24">
-          <h3 class="text-2xl lg:text-3xl font-bold text-white font-display mb-3 lg:mb-5"><span class="text-ccfv-gold">Sign up</span> to learn how you can help</h3>
+          <h3
+            class="text-2xl lg:text-3xl font-bold text-white font-display mb-3 lg:mb-5"
+          >
+            <span class="text-ccfv-gold">Sign up</span> to learn how you can
+            help
+          </h3>
           <!-- first name, last name, email, contact form -->
-          <form name="newsletter-signup" id="newsletter-signup">
-            <div class="grid grid-cols-12 gap-4 ">
+          <form
+            id="ccfv_signup_form"
+            action="https://kulapartners.createsend.com/t/y/s/qsqi/"
+            method="post"
+            @submit.prevent="submitForm"
+          >
+            <div class="grid grid-cols-12 gap-4">
               <div class="col-span-12 lg:col-span-6">
-                <label for="first-name" class="text-white text-lg font-bold hidden">First Name</label>
-                <input type="text" name="first-name" id="first-name" placeholder="First Name" class="w-full p-2 lg:py-3 focus:outline-ccfv-orange focus:ring-0" />
+                <label
+                  for="first-name"
+                  class="text-white text-lg font-bold hidden"
+                  >First Name</label
+                >
+                <input
+                  type="text"
+                  name="cm-name"
+                  id="fullname"
+                  accesskey="n"
+                  placeholder="First Name"
+                  class="w-full p-2 lg:py-3 focus:outline-ccfv-orange focus:ring-0"
+                />
               </div>
               <div class="col-span-12 lg:col-span-6">
-                <label for="last-name" class="text-white text-lg font-bold hidden">First Name</label>
-                <input type="text" name="last-name" id="last-name"  placeholder="Last Name" class="w-full p-2 lg:py-3 focus:outline-ccfv-orange focus:ring-0" />
+                <label
+                  for="fieldmtdtlu"
+                  class="text-white text-lg font-bold hidden"
+                  >First Name</label
+                >
+                <input
+                  type="text"
+                  name="cm-f-mtdtlu"
+                  id="fieldmtdtlu"
+                  placeholder="Last Name"
+                  class="w-full p-2 lg:py-3 focus:outline-ccfv-orange focus:ring-0"
+                />
               </div>
               <div class="col-span-12">
-                <label for="email" class="text-white text-lg font-bold hidden">First Name</label>
-                <input type="text" name="email" id="email"  placeholder="Email" class="w-full p-2 lg:py-3 focus:outline-ccfv-orange focus:ring-0" />
+                <label for="email" class="text-white text-lg font-bold hidden"
+                  >First Name</label
+                >
+                <input
+                  type="text"
+                  name="cm-qsqi-qsqi"
+                  id="email"
+                  placeholder="Email"
+                  class="w-full p-2 lg:py-3 focus:outline-ccfv-orange focus:ring-0"
+                />
               </div>
-              <div class="col-span-12 lg:col-span-8 | grid grid-cols-12 | lg:pe-4">
-                <input type="checkbox" name="consent" id="consent" class="mr-2 mt-1 col-span-3 lg:col-span-1 | focus:ring-0 focus:bg-ccfv-orange checked:bg-ccfv-orange hover:bg-ccfv-orange active:bg-ccfv-orange" />
-                <label class="col-span-9 lg:col-span-11 text-white |  leading-relaxed">
-                  I agree to receive future messages including information about continuing education, upcoming events, vaccine research, and other important news from the Canadian Center for Vaccinology. I will be able to unsubscribe at any time.
+              <div
+                class="col-span-12 lg:col-span-8 | grid grid-cols-12 | lg:pe-4"
+              >
+                <input
+                  type="checkbox"
+                  name="cm-fo-mjjfj"
+                  id="casl-consent"
+                  class="mr-2 mt-1 col-span-3 lg:col-span-1 | focus:ring-0 focus:bg-ccfv-orange checked:bg-ccfv-orange hover:bg-ccfv-orange active:bg-ccfv-orange"
+                />
+                <label
+                  class="col-span-9 lg:col-span-11 text-white | leading-relaxed"
+                >
+                  I agree to receive future messages including information about
+                  continuing education, upcoming events, vaccine research, and
+                  other important news from the Canadian Center for Vaccinology.
+                  I will be able to unsubscribe at any time.
                 </label>
               </div>
-              <div class="col-span-12 lg:col-span-4 ">
-                <button class="border-4 border-white text-white font-bold text-2xl px-4 py-2 lg:py-3 lg:px-6 | hover:bg-ccfv-orange | w-full | mx-auto my-2">Subscribe</button>
+              <div class="col-span-12 lg:col-span-4">
+                <input
+                  type="submit"
+                  class="border-4 border-white text-white font-bold text-2xl px-4 py-2 lg:py-3 lg:px-6 | hover:bg-ccfv-orange | w-full | mx-auto my-2"
+                  value="Subscribe"
+                  accesskey="s"
+                />
               </div>
             </div>
           </form>
+
+          <p v-if="submitted" class="text-ccfv-gold">
+            Thank you for signing up!
+          </p>
         </div>
       </div>
     </div>
---
+++ wp_stuff/page-template-home-2023.php
@@ -0,0 +1,48 @@
+<?php
+/*
+ * Template Name: Temporary Landing Page - 2023
+ * Template Post Type: page
+*
+*/
+  
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <link rel="icon" href="/icon.png" />
+    <link rel="icon" href="/icon.ico" />
+
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Vaccine Research - Clinical Trials | Canadian Center for Vaccinology</title>
+	<meta name="robots" content="index, follow, max-image-preview:large, max-snippet:-1, max-video-preview:-1">
+
+	<!-- This site is optimized with the Yoast SEO plugin v16.1.1 - https://yoast.com/wordpress/plugins/seo/ -->
+	<meta name="description" content="The Canadian Center for Vaccinology is an integrated collaborative vaccine research team committed to research, discovery and evaluation of vaccines.">
+	<link rel="canonical" href="https://centerforvaccinology.ca/">
+	<meta property="og:locale" content="en_US">
+	<meta property="og:type" content="website">
+	<meta property="og:title" content="Vaccine Research - Clinical Trials | Canadian Center for Vaccinology">
+	<meta property="og:description" content="The Canadian Center for Vaccinology is an integrated collaborative vaccine research team committed to research, discovery and evaluation of vaccines.">
+	<meta property="og:url" content="https://centerforvaccinology.ca/">
+	<meta property="og:site_name" content="Canadian Center For Vaccinology">
+	<meta property="article:modified_time" content="2019-02-22T13:39:07+00:00">
+	<script type="application/ld+json" class="yoast-schema-graph">{"@context":"https://schema.org","@graph":[{"@type":"WebSite","@id":"https://centerforvaccinology.ca/#website","url":"https://centerforvaccinology.ca/","name":"Canadian Center For Vaccinology","description":"","potentialAction":[{"@type":"SearchAction","target":"https://centerforvaccinology.ca/?s={search_term_string}","query-input":"required name=search_term_string"}],"inLanguage":"en-CA"},{"@type":"WebPage","@id":"https://centerforvaccinology.ca/#webpage","url":"https://centerforvaccinology.ca/","name":"Vaccine Research - Clinical Trials | Canadian Center for Vaccinology","isPartOf":{"@id":"https://centerforvaccinology.ca/#website"},"datePublished":"2013-12-05T19:59:14+00:00","dateModified":"2019-02-22T13:39:07+00:00","description":"The Canadian Center for Vaccinology is an integrated collaborative vaccine research team committed to research, discovery and evaluation of vaccines.","breadcrumb":{"@id":"https://centerforvaccinology.ca/#breadcrumb"},"inLanguage":"en-CA","potentialAction":[{"@type":"ReadAction","target":["https://centerforvaccinology.ca/"]}]},{"@type":"BreadcrumbList","@id":"https://centerforvaccinology.ca/#breadcrumb","itemListElement":[{"@type":"ListItem","position":1,"item":{"@type":"WebPage","@id":"https://centerforvaccinology.ca/","url":"https://centerforvaccinology.ca/","name":"Home"}}]}]}</script>
+	<!-- / Yoast SEO plugin. -->
+
+
+    <meta name="generator" content="WordPress 5.7">
+    <link rel="shortlink" href="https://centerforvaccinology.ca/">
+    <link rel="alternate" type="application/json+oembed" href="https://centerforvaccinology.ca/wp-json/oembed/1.0/embed?url=https%3A%2F%2Fcenterforvaccinology.ca%2F">
+    <link rel="alternate" type="text/xml+oembed" href="https://centerforvaccinology.ca/wp-json/oembed/1.0/embed?url=https%3A%2F%2Fcenterforvaccinology.ca%2F&amp;format=xml">
+    <script type="module" crossorigin src="/assets/index.js"></script>
+    <link rel="stylesheet" href="/assets/index.css">
+
+
+	</head>
+  <body>
+    <div id="app"></div>
+    
+  </body>
+</html>
